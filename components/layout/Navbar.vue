a7a0fb11d34fd3ab19c541869a48d16dd1e0e487
phamgiang0167 <jagsiph@gmail.com>
2024-05-22 16:33:42 +0700

    creare base code

---
+++ components/layout/Navbar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+// import { userLogout } from "~/composables/useAuth";
+
+// const showSideDrawer = ref(false)
+// const logout = userLogout
+// const router = useRouter()
+// const colorMode = useColorMode()
+
+// const user = useState('user')
+// const initalCheck = await useLoggedIn()
+// const isLoggedIn = ref(initalCheck)
+
+// router.afterEach(() => {
+//   showSideDrawer.value = false
+// })
+
+// const setColorTheme = (newTheme: Theme) => {
+//   colorMode.preference = newTheme
+// }
+
+// async function checkIfLoggedIn() {
+//   const check = await useLoggedIn()
+//   isLoggedIn.value = check
+// }
+
+// watch(user, async () => {
+//   await checkIfLoggedIn()
+// }, { deep: true });
+
+</script>
+
+<template>
+  <div class="navbar relative dark:bg-black">
+    aaaa
+
+  </div>
+
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navbar from "@/components/layout/Navbar.vue";
+// import TheFooter from "~~/components/layout/TheFooter.vue";
+</script>
+
+<template>
+  <div class="dark:bg-black">
+    <Navbar/>
+      <slot/>
+      <TheFooter/>
+  </div >
+</template> 
