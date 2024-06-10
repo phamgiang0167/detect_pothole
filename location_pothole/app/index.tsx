0e0e2de12758b0ae8b882bf87903647b0f569efe
phamgiang0167 <jagsiph@gmail.com>
2024-06-10 22:42:16 +0700

    desg: layout FE

---
+++ location_pothole/app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from 'react-native';
+
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+    <View>
+      <Link href="/about">About</Link>
+      {/* ...other links */}
+      <Link href="/user/bacon">View user</Link>
+    </View>
+  );
+}
